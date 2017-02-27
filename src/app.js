eb41a5d770e7d69eaf74a0773ab4703f714f28d6
James Cool <jeffreysbrother@gmail.com>
2017-02-27 14:15:21 -0800

    basic stuff

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import Map from "can/map/";
+import route from "can/route/";
+import 'can/map/define/';
+import 'can/route/pushstate/';
+
+const AppViewModel = Map.extend({
+  define: {
+    message: {
+      value: 'Hello World!',
+      serialize: false
+    },
+    title: {
+      value: 'donejs-chat',
+      serialize: false
+    }
+  }
+});
+
+route('/:page', { page: 'home' });
+
+export default AppViewModel;
